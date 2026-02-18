86eb6c359ec2ff4de044ced633acda921758ce58
Rian Ganesh <rianganesh64@gmail.com>
2026-02-18 15:41:01 -0500

    Add files via upload
    After programming stepper, max RPM of around 15 not enough, (takes around 1 min for 1.5 meters of lowering). Swapping to continuous rotation stepper motor, much higher max RPM, initial code here.

---
+++ crane_arduino_servo_v1/crane_arduino_servo_v1.ino
@@ -0,0 +1,13 @@
+#include <Servo.h>
+
+Servo myservo; // create servo object to control servo
+
+int pos = 0; // variable to store servo position
+
+void setup() [
+  myservo.attach(9); // attaches to pin 9 on the servo object
+]
+
+void loop() [
+  
+]
