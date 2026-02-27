a50e0e773bab8df1706c28845fed73296c54403e
Rian Ganesh <rianganesh64@gmail.com>
2026-02-27 14:43:43 -0500

    Refactor servo control logic and add button handling
    Modified code to incorporate input from push button. Added logic to change input from time to meters

--- crane_arduino_servo_v2.ino
+++ crane_arduino_servo_v2.ino
@@ -1,31 +1,42 @@
-#include <Servo.h>
+#include <Servo.h> //calls servo library
+Servo myServo; //creates object for servo motor
+const int servoPin = 8; // servo pin on arduino
+const int buttonPin = 10; //button pin on arduino
+int runFlag = 0; // start state at 0
 
-Servo myServo;
-
-const int servoPin = 8;
-Servo myservo;
-
-
-
-//USER VARIABLES
-int runTime = 500000;     // milliseconds
-int speedValue = 180;   // 180 is down, 0 is up
-int stopValue = 90;     // calibrated stop value, (90 is current value)
+// Variables
+int speedValue = 180;       // 180 is down, 0 is up
+int stopValue = 90;         // calibrated stop position
+const float secondsPerMeter = 5.0; // assumes that 1 meter = 5 seconds, WILL NEED TO ADJUST
+float metersPerRun = 2.3; // distance want to go (meters)
 
 void setup() {
   myServo.attach(servoPin);
-
-  runServo(speedValue, runTime);
-
-  myServo.write(stopValue);  // ensure stop
+  myServo.write(stopValue); // Start in stopped position
+  
+  pinMode(buttonPin, INPUT_PULLUP);
+  runFlag = 0; //Flag starts at zero, not moving
 }
 
-
 void loop() {
-  // empty, doesn't loop
+
+  if (digitalRead(buttonPin) == LOW) {
+    delay(50); // debouncing to prevent double click errors
+    runFlag = 1; // when button pressed, Flag goes to 1
+  }
+
+  if (runFlag == 1) {
+    // Distance (m) -> time (ms): timeMs = meters * (metersPerRun sec/m) * (1000 ms/sec)
+    int runTimeMs = (int)(metersPerRun * secondsPerMeter * 1000.0);
+
+    runServo(speedValue, runTimeMs); //if Flag=1, run motor
+    
+    myServo.write(stopValue); // stopping servo
+    runFlag = 0;              // Reset so it waits for next press
+  }
 }
 
 void runServo(int speedCmd, int durationMs) {
   myServo.write(speedCmd);
-  delay(durationMs);
-} // creates custom function used in the void setup()*/
+  delay(durationMs); // keeps servo moving for set duration
+} //custom function
